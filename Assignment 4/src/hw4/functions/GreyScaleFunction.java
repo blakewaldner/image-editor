92ba4bd63554599d3f5f0996caa21bd1b4418c3c
hypecooks <blakewaldner@gmail.com>
2021-11-09 10:57:01 -0500

    Javadocs

--- Assignment 4/src/hw4/functions/GreyScaleFunction.java
+++ Assignment 4/src/hw4/functions/GreyScaleFunction.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 
 import hw4.ImageModel;
 
+/**
+ * This class represents the command for greyscaling an image. It performs luma greyscaling
+ * using a kernal on a given image to a new given image name. If same image name,
+ * the image is overridden with the new image.
+ */
 public class GreyScaleFunction extends AColorTransform {
 
+  /**
+   * Constructor for creating greyscale function object.
+   * Takes no arguments, command for activating function is hard coded in
+   * and set to abstract parent function.
+   */
   public GreyScaleFunction() {
     super("greyscale", new double[][]{
             new double[]{0.2126, 0.7152, 0.0722},
